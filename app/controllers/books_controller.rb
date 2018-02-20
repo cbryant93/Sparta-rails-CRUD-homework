11401fd44f78c1e9f69b6a08325138305f84a575
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-02-20 21:58:06 +0000

    forms working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = Book.new(book_params)
-    
+
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
@@ -70,6 +70,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.fetch(:book, {})
+      params.require(:book).permit(:name, :summary, :author_id)
     end
 end
